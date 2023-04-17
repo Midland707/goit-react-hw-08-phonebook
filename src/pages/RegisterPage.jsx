39243400fe46a89add80e7b6bd540f01de561778
Midland707 <rus14job@ukr.net>
2023-04-17 12:00:12 +0300

    add form to login and register pages

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,31 @@
+import { Input, Button } from '@chakra-ui/react';
+import { NavLink } from 'react-router-dom';
+
 const RegisterPage = () => {
-  return <>Register</>;
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <label>
+          Username
+          <Input type="text" name="name" />
+        </label>
+        <label>
+          Email
+          <Input type="email" name="email" />
+        </label>
+        <label>
+          Password
+          <Input type="password" name="password" />
+        </label>
+        <Button type="submit">Register</Button>
+      </form>
+      <NavLink to={`/login`}>Log In</NavLink>
+    </>
+  );
 };
 
 export default RegisterPage;
