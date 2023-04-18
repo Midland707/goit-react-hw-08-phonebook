09c30bbb9b4d676e695755f8fb983dc73087264b
Midland707 <109915910+Midland707@users.noreply.github.com>
2023-04-18 15:14:47 +0000

    Deploying to gh-pages from @ Midland707/goit-react-hw-08-phonebook@e641e2b4bed7ccc28d5df4eebbcb75dbb63bfac8 🚀

---
+++ static/js/983.cea961de.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[983],{3983:function(e,n,o){o.r(n);var t=o(3329);n.default=function(){return(0,t.jsx)(t.Fragment,{children:"Home"})}}}]);
+//# sourceMappingURL=983.cea961de.chunk.js.map
