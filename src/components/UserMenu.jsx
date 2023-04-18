d17d2b2e8409e64fafd9f6a4ba72cda1f26db60f
Midland707 <rus14job@ukr.net>
2023-04-18 23:16:44 +0300

    bugfix logOut button

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,9 +1,12 @@
 import { ListItem, Button } from '@chakra-ui/react';
+import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/authorization/operations';
 
 export const UserMenu = () => {
+  const dispatch = useDispatch();
+
   const handleClick = () => {
-    logOut();
+    dispatch(logOut());
   };
   return (
     <ListItem>
