d97ba3256f92336e31b4f42e139e990283d3a07d
Midland707 <rus14job@ukr.net>
2023-04-19 01:03:26 +0300

    add title to email

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -38,7 +38,7 @@ export const RegisterForm = () => {
             type="email"
             name="email"
             pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            title="email@dnsname"
             required
           />
         </label>
