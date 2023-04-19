bc54e55772b165c3763857d9feaf010175d4d1bf
Midland707 <rus14job@ukr.net>
2023-04-20 01:56:36 +0300

    refactoring LoginForm to Formik

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,12 @@
 import {
   Input,
   Button,
-  Box,
-  Text,
   FormLabel,
   FormControl,
+  Box,
+  Text,
 } from '@chakra-ui/react';
+import { Field, Form, Formik } from 'formik';
 
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/authorization/operations';
@@ -13,18 +14,6 @@ import { logIn } from 'redux/authorization/operations';
 export const LoginForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    console.log('form = ', form);
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
-  };
   return (
     <Box w="500px" mr="auto" ml="auto">
       <Text color="white" fontSize="50px" textAlign="center">
@@ -33,46 +22,73 @@ export const LoginForm = () => {
       <Text color="white" fontSize="20px" textAlign="center">
         Sign in and start managing your contacts!
       </Text>
-      <FormControl
-        onSubmit={handleSubmit}
-        autoComplete="off"
-        color="white"
-        w="300px"
-        mr="auto"
-        ml="auto"
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            dispatch(
+              logIn({
+                email: values.email,
+                password: values.password,
+              })
+            );
+            actions.setSubmitting(false);
+            actions.resetForm();
+          }, 1000);
+        }}
       >
-        <FormLabel margin="0px" marginTop="20px">
-          Email
-          <Input
-            placeholder=". . . email"
-            type="email"
-            name="email"
-            pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-            title="email@dnsname"
-            required
-          />
-        </FormLabel>
-        <FormLabel margin="0px" marginTop="20px">
-          Password
-          <Input
-            placeholder=". . . password"
-            type="password"
-            name="password"
-            pattern=".{6,}"
-            title="Six or more characters"
-            required
-          />
-        </FormLabel>
-        <Button
-          type="submit"
-          w="300px"
-          marginTop="20px"
-          color="white"
-          bg="#20DF7F"
-        >
-          Log In
-        </Button>
-      </FormControl>
+        {props => (
+          <Form>
+            <Box color="white" w="300px" mr="auto" ml="auto">
+              <Field name="email">
+                {({ field }) => (
+                  <FormControl>
+                    <FormLabel margin="0px" marginTop="20px">
+                      Email
+                      <Input
+                        {...field}
+                        placeholder=". . . email"
+                        type="email"
+                        name="email"
+                        pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                        title="email@dnsname.com"
+                        required
+                      />
+                    </FormLabel>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="password">
+                {({ field }) => (
+                  <FormControl>
+                    <FormLabel margin="0px" marginTop="20px">
+                      Password
+                      <Input
+                        {...field}
+                        placeholder=". . . password"
+                        type="password"
+                        name="password"
+                        pattern=".{6,}"
+                        title="Six or more characters"
+                        required
+                      />
+                    </FormLabel>
+                  </FormControl>
+                )}
+              </Field>
+              <Button
+                type="submit"
+                w="300px"
+                marginTop="20px"
+                color="white"
+                colorScheme="green"
+              >
+                Log In
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
     </Box>
   );
 };
