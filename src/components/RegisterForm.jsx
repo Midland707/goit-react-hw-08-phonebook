f9f9adcd9b7d2a9b6cdd34645ef0fdd2357d49a7
Midland707 <rus14job@ukr.net>
2023-04-20 02:13:51 +0300

    bugfix RegisterForm

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -13,19 +13,6 @@ import { register } from 'redux/authorization/operations';
 export const RegisterForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
-  };
-
   return (
     <Box w="500px" mr="auto" ml="auto">
       <Text color="white" fontSize="50px" textAlign="center">
